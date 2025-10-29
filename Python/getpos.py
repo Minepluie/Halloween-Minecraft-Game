73112e1934f8938ebb37a50ae24c391eebf68e1a
Minepluie <126269059+Minepluie@users.noreply.github.com>
2025-10-29 13:51:10 +0100

    Delete Python/getpos.py

--- Python/getpos.py
+++
@@ -1,8 +0,0 @@
-import mcpi.minecraft as minecraft
-mc = minecraft.Minecraft.create()
-
-while True:
-    if mc.events.pollChatPosts():
-        pos = mc.player.getTilePos()
-        mc.postToChat(f"{pos.x,pos.y,pos.z}")
-        print(f"{pos.x,pos.y,pos.z}")
